1a4aaf40f205164f989de71979ed8b44cdd84cd4
Daniel Guinter <guinterdaniel@gmail.com>
2022-05-01 14:18:42 -0300

    Backend project created

---
+++ backend/src/main/java/com/danielguinter/damicarveiculos/DamicarveiculosApplication.java
@@ -0,0 +1,13 @@
+package com.danielguinter.damicarveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamicarveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamicarveiculosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielguinter/damicarveiculos/DamicarveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielguinter.damicarveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamicarveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
