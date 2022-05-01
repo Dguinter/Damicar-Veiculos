3f3d66b3e89adb11d10d698cc8eef2b25b695996
Daniel Guinter <guinterdaniel@gmail.com>
2022-05-01 18:53:02 -0300

    Connection H2 database

--- backend/src/main/java/com/danielguinter/damicarveiculos/resources/CategoryResource.java
+++ backend/src/main/java/com/danielguinter/damicarveiculos/resources/CategoryResource.java
@@ -1,24 +1,26 @@
 package com.danielguinter.damicarveiculos.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danielguinter.damicarveiculos.entities.Category;
+import com.danielguinter.damicarveiculos.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Sendan"));
-		list.add(new Category(2L, "SUV"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/danielguinter/damicarveiculos/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.danielguinter.damicarveiculos.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielguinter.damicarveiculos.entities.Category;
+import com.danielguinter.damicarveiculos.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	public List<Category> findAll(){
+		return repository.findAll(); 
+	}
+
+}
