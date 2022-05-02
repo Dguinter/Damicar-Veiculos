17db0a2e2df56067959312a8b716e1e5e4ee6186
Daniel Guinter <guinterdaniel@gmail.com>
2022-05-01 22:31:48 -0300

    Created DTO

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_category(name) VALUES ('Hatch');
+INSERT INTO tb_category(name) VALUES ('SUV');
+INSERT INTO tb_category(name) VALUES ('Sedan');
+INSERT INTO tb_category(name) VALUES ('Van');
+INSERT INTO tb_category(name) VALUES ('Picape');
+INSERT INTO tb_category(name) VALUES ('Off-road');
+INSERT INTO tb_category(name) VALUES ('Minivan');
+INSERT INTO tb_category(name) VALUES ('Crossover');
