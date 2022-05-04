401670cbe3d0b878780108343e09f84ccbcc6228
Daniel Guinter <guinterdaniel@gmail.com>
2022-05-04 00:45:04 -0300

    Product GET ok

---
+++ backend/src/main/java/com/danielguinter/damicarveiculos/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.danielguinter.damicarveiculos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielguinter.damicarveiculos.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
